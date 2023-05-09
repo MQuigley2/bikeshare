8e68b249fc70f9c7606e37118069df6342b997c9
Michael Quigley <michaeljquigley7@gmail.com>
2023-05-08 21:22:30 -0500

    Refactored conection functions with decorator

--- connection_funcs.py
+++ connection_funcs.py
@@ -13,8 +13,31 @@ db_connection_str = f'mysql+mysqlconnector://root:{mysql_pass}@localhost/bike_sh
 
 
 
-def exec_query(query):
-    return pd.read_sql(query, con=db_connection_str)
+def establish_connection(func):
+    def wrapper(*args,**kwargs):
+
+        try:
+            connection = mysql.connector.connect(user='root',
+                                                password=mysql_pass,
+                                                host='localhost',
+                                                database='bike_share')
+            result=func(connection,*args,**kwargs)
+            connection.close()
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            connection.close()
+        return result
+    return wrapper
+
+
+def exec_query(query,*args,**kwargs):
+    return pd.read_sql(query, con=db_connection_str,*args,**kwargs)
 #Runs SQL query on bikeshare database and returns pandas df
 
 
@@ -47,3 +70,18 @@ def run_sql(query):
     else:
         connection.close()
 #More generalized function for DDL and DML statements. Does not return any output
+
+
+@establish_connection
+def run_multiple_sql(connection,queries):
+
+    for query in queries[0:-1]:
+        cursor=connection.cursor()
+        cursor.execute(query)
+        cursor.close()
+    cursor=connection.cursor()
+    cursor.execute(queries[-1])
+    result=pd.DataFrame(cursor.fetchall())
+    cursor.close()
+    return result
+
